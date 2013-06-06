dc0e130aee70164217ce44f4d3e219a22f0e91a2
Steven Myint <git@stevenmyint.com>
2013-06-05 19:14:47 -0700

    Add quotes

--- test.bash
+++ test.bash
@@ -2,8 +2,8 @@
 
 for expected in test/*_expected.tex
 do
-    base=$(basename $expected _expected.tex)
-    ./rst2beamer.py test/$base.rst .tmp.tex
-    diff $expected .tmp.tex
+    base=$(basename "$expected" '_expected.tex')
+    ./rst2beamer.py "test/$base.rst" .tmp.tex
+    diff "$expected" .tmp.tex
     rm -f .tmp.tex
 done
