7b4aa9b1ecf523b64dfafbf2e9d0cbff8d6756a8
Steven Myint <git@stevenmyint.com>
2013-08-22 21:54:23 -0700

    Update due to changes in docutils 0.11

--- test.bash
+++ test.bash
@@ -4,6 +4,8 @@ for expected in test/*_expected.tex
 do
     base=$(basename "$expected" '_expected.tex')
     ./rst2beamer.py "test/$base.rst" .tmp.tex
-    diff "$expected" .tmp.tex
+    diff --unified "$expected" .tmp.tex
     rm -f .tmp.tex
 done
+
+echo -e '\x1b[01;32mOkay\x1b[0m'
