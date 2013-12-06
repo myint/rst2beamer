010450e0be2192723fe7fbae204faa696e984f3b
Steven Myint <git@stevenmyint.com>
2013-12-06 05:13:56 -0800

    Format

--- rst2beamer.py
+++ rst2beamer.py
@@ -1008,9 +1008,11 @@ class BeamerTranslator(LaTeXTranslator):
         attrs = node.attributes
         if not 'align' in attrs and self.centerfigs:
             attrs['align'] = 'center'
-        if (('height' not in attrs) and
+        if (
+            ('height' not in attrs) and
             ('width' not in attrs) and
-            ('scale' not in attrs)):
+            ('scale' not in attrs)
+        ):
             attrs['height'] = '0.75\\textheight'
         LaTeXTranslator.visit_image(self, node)
 
