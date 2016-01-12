20f7fe05256b7e9b956756ce5922ae01256d0495
Steven Myint <git@stevenmyint.com>
2016-01-11 18:13:27 -0800

    Increment patch version to 0.9.1

--- rst2beamer.py
+++ rst2beamer.py
@@ -56,7 +56,7 @@ from docutils.writers.latex2e import PreambleCmds
 __docformat__ = 'restructuredtext en'
 __author__ = ('Ryan Krauss <ryanwkrauss@gmail.com> & '
               'Paul-Michael Agapow <agapow@bbsrc.ac.uk>')
-__version__ = '0.9'
+__version__ = '0.9.1'
 
 
 SHOWNOTES_FALSE = 'false'
